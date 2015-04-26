5d4aef43708f5f40d5adaf2efd21698831c9bdf4
Ivan Dunin <johnmoony@mail.ru>
2015-04-26 22:30:17 +0300

    Sass initial.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,46 @@
 module.exports = function (grunt) {
-		grunt.initConfig({
-				shell: {
-						options: {
-								stdout: true,
-								stderr: true
-						},
-						server: {
-								command: 'java -cp Cassandra.jar main.Main 8100'
-						}
+	grunt.initConfig({
+
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
 				},
+				files: {
+					'public_html/css/main.css': 'public_html/sass/main.scss'					
+				}
+			}
+		},	
+
+		shell: {
+				options: {
+						stdout: true,
+						stderr: true
+				},
+				server: {
+						command: 'java -cp Cassandra.jar main.Main 8100'
+				}
+		},
+
 		fest: {
 				templates: {
-	          files: [{
-	              expand: true,
-	              cwd: 'templates',
-	              src: '*.xml',
-	              dest: 'public_html/js/tmpl'
-	          }],
-	          options: {
-	              template: function (data) {
-	                  return grunt.template.process(
-	                      'define(function () { return <%= contents %> ; });',
-	                      { data: data }
-	                  );
-	              }
-	          }
+					files: [{
+						expand: true,
+						cwd: 'templates',
+						src: '*.xml',
+						dest: 'public_html/js/tmpl'
+					}],
+					options: {
+						template: function (data) {
+							return grunt.template.process(
+								'define(function () { return <%= contents %> ; });',
+								{ data: data }
+							);
+						}
+					}
 	      }
 		},
+
 		watch: {
             fest: {
                 files: ['templates/*.xml'],
@@ -39,13 +53,16 @@ module.exports = function (grunt) {
             server: {
                 files: [
                     'public_html/js/**/*.js',
-                    'public_html/css/**/*.css'
+                    'public_html/css/**/*.css',
+                    'public_html/sass/**/*.scss'
                 ],
+                tasks: ['sass'],
                 options: {
                     livereload: true
                 }
             }
         },
+
         concurrent: {
             target: ['watch', 'shell'],
             options: {
@@ -55,9 +72,11 @@ module.exports = function (grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-shell');
 	grunt.loadNpmTasks('grunt-fest');
+
 
 	grunt.registerTask('default', ['concurrent']);
 };
