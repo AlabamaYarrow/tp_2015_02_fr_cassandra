05b5dae920f9820868323a5026c3a623091b07fe
Ivan Dunin <johnmoony@mail.ru>
2015-05-13 13:41:08 +0300

    Dynamic drawing.

--- public_html/js/views/paintarea.js
+++ public_html/js/views/paintarea.js
@@ -63,6 +63,9 @@ define([
             var gameDiv = $('.game').parent();
             gameDiv.removeAttr('style');
             this.calculateOffset();
+            this.canvas2 = $('.js-canvas2');
+            this.canvas2.get(0).width = $('.paintarea').width();
+            this.canvas2.get(0).height = $('.paintarea').height();
         },
 
         hide: function() {
@@ -87,6 +90,9 @@ define([
             this.curve.width = this.context.lineWidth;
             this.curve.start = {'x': x, 'y': y};
             this.curve.lines = [];
+
+            this.prevX = x;
+            this.prevY = y;
 
             this.context.moveTo(x, y);
             this.context.beginPath();
@@ -121,8 +127,18 @@ define([
             var y = e.pageY - this.offsetTop;            
             this.drawLine(x,y);            
 
+
             var line = {'x': x, 'y': y};
             this.curve.lines.push(line);
+            this.curve.start = {'x': this.prevX, 'y': this.prevY};
+
+            this.prevX = x;
+            this.prevY = y;
+
+            data = JSON.stringify(this.curve);
+            this.drawOnOtherCanvas(data);
+            this.curve.lines = [];
+
         },
 
         drawLine: function (x,y) {
@@ -139,18 +155,13 @@ define([
             }
             this.context.closePath();
 
-            data = JSON.stringify(this.curve);
-            this.drawOnOtherCanvas(data);
 
         },
 
         drawOnOtherCanvas: function (data) {
-            canvas = $('.js-canvas2');
-            canvas.get(0).width = $('.paintarea').width();
-            canvas.get(0).height = $('.paintarea').height();
-            context = canvas.get(0).getContext('2d');
+            context = this.canvas2.get(0).getContext('2d');
 
-            var canvasRectangle = canvas.get(0).getBoundingClientRect();
+            var canvasRectangle = this.canvas2.get(0).getBoundingClientRect();
             offsetLeft = canvasRectangle.left + window.scrollX;
             offsetTop = canvasRectangle.top + window.scrollY;
 
@@ -159,7 +170,7 @@ define([
 
             if (curve.clear) {
                 context.fillStyle = '#FFFFFF';
-                context.fillRect(0, 0, canvas.width(), canvas.height());
+                context.fillRect(0, 0, this.canvas2.width(), this.canvas2.height());
                 return;
             }
 
